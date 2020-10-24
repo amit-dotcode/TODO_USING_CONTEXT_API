85a9aaf9520a45cb48bad722e4c4ac37a742b8f8
amit-dotcode <amitc7725@gmail.com>
2020-10-24 10:10:56 +0530

    upload git hub

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/index.css';
-import Header from "./component/Header";
-import Tabledata from './component/Tabledata';
-import SwitchToggle from './component/Switchtoggle';
-import {Provider} from 'react-redux';
-import store from './Store';
-import Addcontact from './component/Addcontact';
-import Editcontact from './component/EditContact';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
 const App = () => {
    
     return (
-      <Provider store={store}>
-        {/* Using Route  */}
-        <BrowserRouter>
-        <Header/>  
-        <div className="container my-5">
-        <SwitchToggle/>  
-        <Switch>
-          <Route exact path="/" component={Tabledata}/>
-          <Route exact path="/component/add" component={Addcontact}/> 
-          <Route exact path="/component/edit/:id" component={Editcontact}/> 
-        </Switch>  
-        </div> 
-        </BrowserRouter>
-
-      </Provider>
+      <>
+        <h4>New Project In react </h4>
+      </>
     );
 
 
